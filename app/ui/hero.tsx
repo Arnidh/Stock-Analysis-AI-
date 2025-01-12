c918687c403b93da8287e008a3c3f9c36fc2b6ff
Mehul Goyal <145589189+Arnidh@users.noreply.github.com>
2025-01-12 19:16:34 +0530

    Delete app directory

--- app/ui/hero.tsx
+++
@@ -1,8 +0,0 @@
-export default function Hero() {
-    return (
-        <div>
-            <h1 className="text-center text-5xl font-sans bg-gradient-to-r from-yellow-400 to-orange-700 p-5 rounded">Stock Analysis AI</h1>
-            <p className="text-center mt-5 font-semibold text-2xl">Unlock the Future of investing with AI</p>
-        </div>
-    )
-}
